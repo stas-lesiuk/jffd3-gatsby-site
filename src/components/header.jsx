f0fd5aff98eb61764ff5334fe6321c3a27346985
stas-lesiuk <stanislav.lesiuk@gmail.com>
2021-07-07 12:04:25 +0200

    Challenge 4 - add first plugin & use StaticImage

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image'
 
-import { headerLink } from './header.module.css';
+import { headerLink, logo } from './header.module.css';
 
 const Header = () => {
     return (
         <div>
+            <StaticImage className={logo} src="../images/icon.png" alt="My Awesome Website logo" />
             <Link className={headerLink} to="/">Strona główna</Link>
             <Link className={headerLink} to="/about">About</Link>
             <Link className={headerLink} to="/news-page">Nowości / Blog</Link>
